3f428f0d7467f0ec348382edc3a41ec4b1b71a13
plipbip <71358503+plipbip@users.noreply.github.com>
2020-10-15 17:56:23 -0500

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,31 @@
+class Chain{
+
+    constructor(bodyA,bodyB){
+    var options = {
+    bodyA:bodyA,
+    bodyB:bodyB,
+    length:10,
+    stiffness:0.5
+    }
+    this.chain=Constraint.create(options);
+    World.add(world,this.chain);
+    
+     
+
+    }
+    display(){
+    var posA=this.chain.bodyA.position
+    var posB=this.chain.bodyB.position    
+    strokeWeight(5);
+    line(posA.x,posA.y,posB.x,posB.y)
+
+
+    }
+    
+
+
+
+
+
+
+};
